e63e0e9d6a017f4bb92127fa75a556256df597ab
ryanKor <equus3144@gmail.com>
2021-07-25 02:22:24 +0900

    amazon interview algorithm preparation

---
+++ leet_code/07_reverse_integer.py
@@ -0,0 +1,40 @@
+class Solution:
+    def reverse(self, x: int) -> int:
+        # solution 1
+        result = ''
+        if x < 0:
+            result = str(x)[0] + str(x)[1:][::-1]
+        else:
+            result = str(x)[::-1]
+
+        if int(result) < -2**31 or int(result) > 2**31 - 1:
+            return 0
+        return int(result)
+
+
+# solution 2
+#         newArr = []
+#         isNegative = False
+
+#         if (x < 0):
+#             isNegative = True
+#             x = -x
+
+#         while (x > 0):
+#             lastDigit = x % 10
+#             newArr.insert(0, lastDigit)
+#             x //= 10
+
+#         result = 0
+#         multiplier = 1
+#         for num in newArr:
+#             result += num  * multiplier
+#             multiplier *= 10
+
+#         if isNegative:
+#             result = -result
+
+#         if (result < -2**31 or result > 2**31-1):
+#             return 0
+
+#         return result
